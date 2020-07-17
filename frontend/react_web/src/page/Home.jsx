6e1ed50aadf2dea34a4e0b694b48f4c680234059
Soohyun <33894603+1211sh@users.noreply.github.com>
2020-07-17 23:12:31 +0900

    update

--- frontend/react_web/src/page/Home.jsx
+++ frontend/react_web/src/page/Home.jsx
@@ -1,11 +1,19 @@
+import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import React from 'react'
+import { ScrollingProvider, Section } from 'react-scroll-section'
 
 import Inference from './inference/Inference'
 import Intro from './intro/Intro'
-import TitleBar from './titlebar/TitleBar'
 import PhotoPage from './photoPage/PhotoPage'
 import TimeLine from './timeline/TimeLine'
+import { Item } from './titlebar/Menu'
+import SectionContainer from './titlebar/SectionContainer'
+import TitleBar from './titlebar/TitleBar'
+
+import imgLamp from '../assets/magic-lamp.png'
+import imgTeam from '../assets/weareteam.png'
+
 
 const useStyles = makeStyles({
   root: {
@@ -20,35 +28,46 @@ const useStyles = makeStyles({
 
 export default function Home() {
   const classes = useStyles()
-  //const focusName = useState('intro')
-  //const ref = useRef()
-
-  // const handleFocus = useCallback((event) => {
-  //   event.preventDefault()
-  //   switch(param){
-  //     case '':
-  //       return <Intro/>
-  //     case '':
-  //       return <Inference/>
-  //     case '':
-  //       return <TimeLine/>
-  //     case '':
-  //       return <PhotoPage/>
-  // }
-  //   ref.focus()
-  // }, [ref1, ref2, ref3, ref4])
-
-  
 
   return (
-    <div className={classes.root}>
-      <TitleBar />
+    <ScrollingProvider scrollBehavior="smooth" className={classes.root}>
+      <TitleBar>
+        {/* <Typography className={classes.lampImg} src={imgLamp} component='img' ></Typography> */}
+        <Item section='genie'>
+          <Typography className={classes.lampImg} src={imgLamp} component='img' width='50' ></Typography>
+        </Item>
+        <Item section='inference'>
+          <Typography className={classes.typography}>INFERENCE</Typography>
+        </Item>
+        <Item section='introduce'>
+          <Typography className={classes.typography}>INTRODUCE</Typography>
+        </Item>
+        <Item section='member'>
+          <Typography className={classes.lampImg} src={imgTeam} component='img' width='50' ></Typography>
+        </Item>
+      </TitleBar>
       <div className={classes.content}>
-        <Intro />
-        <Inference />
-        <TimeLine />
-        <PhotoPage />
+        <Section id='genie'>
+          <SectionContainer>
+            <Intro />
+          </SectionContainer>
+        </Section>
+        <Section id='inference'>
+          <SectionContainer>
+            <Inference />
+          </SectionContainer>
+        </Section>
+        <Section id='introduce'>
+          <SectionContainer>
+            <TimeLine />
+          </SectionContainer>
+        </Section>
+        <Section id='member'>
+          <SectionContainer>
+            <PhotoPage />
+          </SectionContainer>
+        </Section>
       </div>
-    </div>
+    </ScrollingProvider>
   )
 }
